582131f8c89234b2899bec01a346e0b3955773e7
Matt Nudo <matt.nudo@gmail.com>
2025-04-13 17:02:28 -0400

    some new solutions

--- python/src/arrays/7.py
+++ python/src/arrays/7.py
@@ -1,23 +1,8 @@
-#https://leetcode.com/problems/reverse-integer/description/
-
-def reverse(x: int) -> int:
-    string = str(x)
-    NEGATIVE_SIGN = "-"
-    negative = string[0] == NEGATIVE_SIGN
-    if(negative):
-        string = string[1:]
-    
-    string = string[::-1]
-    integer = int(string)
-    string = str(integer)
-    return string if not negative else NEGATIVE_SIGN+string
-    
-    
-
-print(reverse(123))
-print(reverse(-123))
-print(reverse(120))
-
-#consider negatives 
-#consider trailing 0s
-#strip sign, convert to string. reverse string. add sign. 
+class Solution:
+    def reverse(self, x: int) -> int:
+       
+        string = str(x)[::-1]
+        result = int(string) if string[-1] != '-' else int("-"+string[:len(string)-1])
+        if(result >= math.pow(2,31)-1 or result <= math.pow(-2,31)):
+            return 0
+        return result
